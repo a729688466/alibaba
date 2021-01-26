cf47b7bffe24cddb97f713ab84a81b3314dfa083
a729688466 <729688466@qq.com>
2021-01-26 21:52:49 +0800

    log

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>hello,react</div>
+             </header>
+    </div>
+  );
+}
+
+//暴露App组件
+export default App;
